ed7502248f4527910f9005f790506c59f5b306d8
vardan13 <vardanhingmire13@gmail.com>
2021-08-16 10:38:21 +0000

    Add files via upload

---
+++ Cust_Auth/models/User.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var UserSchema = new Schema({
+    username: String,
+    email:String,
+    password: String,
+    name: String,
+
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
